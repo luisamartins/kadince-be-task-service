9f1118136ca8aae1390c17af2bd8c7aae6e9a91c
Luisa Souza <luisa.martins@benfatto.net.br>
2025-01-26 18:09:03 -0300

    [FEATURE-26] Adding documentation to backend

--- src/main/java/com/kadince/task/repository/TaskRepository.java
+++ src/main/java/com/kadince/task/repository/TaskRepository.java
@@ -7,8 +7,16 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * Repository interface for Task entity.
+ */
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    /**
+     * Retrieves all tasks ordered by deadline and completion status.
+     *
+     * @return a list of tasks
+     */
     @Query("SELECT t FROM Task t ORDER BY t.completed ASC, CASE WHEN t.deadline IS NULL THEN 1 ELSE 0 END, t.deadline ASC")
     List<Task> findAllOrderByDeadlineAndCompleted();
 }
